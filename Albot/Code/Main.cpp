ddfc6411f185055b213ade1530d80e6e0f0685f6
Jose Rosenbluth <jnrosenb@uc.cl>
2019-09-19 02:26:07 -0700

    Added files into project settings

--- Albot/Code/Main.cpp
+++ Albot/Code/Main.cpp
@@ -4,6 +4,18 @@
 #include "Managers/FrameManager.h"
 #include "CantDebug/CantDebug.h"
 
+
+#ifdef DEVELOPER
+#include "Factory/Factory.h"
+#include "Managers/SystemManager.h"
+#include "Managers/GameObjectManager.h"
+
+GameObjectManager *goMgr;
+SystemManager *sysMgr;
+Factory *factory;
+#endif
+
+
 int main()
 {
 	// Create Window
@@ -16,6 +28,14 @@ int main()
 	SDL_Event quit_event;
 	bool done = false;
 	float dt;
+
+
+	#ifdef DEVELOPER
+	goMgr = new GameObjectManager();
+	sysMgr = new SystemManager();
+	factory = new Factory();
+	#endif
+
 
 	while (!done)
 	{
@@ -33,11 +53,24 @@ int main()
 		DEBUG_TRACE("Frame Time: %f ms", dt);
 		DEBUG_TRACE("Framerate: %f Hz", 1000.f/dt);
 
+		
+		#ifdef DEVELOPER
+		goMgr->ProcessQueues();
+		sysMgr->Update(dt);
+		#endif
+
+		
 		DEBUG_UPDATE
 		frame_manager.EndFrame();
 	}
 
 	DEBUG_CLOSE;
+
+	#ifdef DEVELOPER
+	delete goMgr;
+	delete sysMgr;
+	delete factory;
+	#endif
 
 	return 0;
 }
