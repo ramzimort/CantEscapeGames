8848bd16de93dd88102a69cab0552863ee95fbd3
theAlekseyP <32024066+theAlekseyP@users.noreply.github.com>
2019-11-26 18:36:20 -0800

    added CollisionMask to scripting (#16)

--- Albot/Scripts/Components/TriggerTest.lua
+++ Albot/Scripts/Components/TriggerTest.lua
@@ -41,7 +41,29 @@ end
 
 
 TriggerTest.OnEnter = function(self, gameObj1, gameObj2)
-	OutputPrint("OnEnter");
+	local triggerComp1 = gameObj1:GetTriggerComp();
+	local triggerComp2 = gameObj2:GetTriggerComp();
+
+	if (triggerComp1 == nil or triggerComp2 == nil) then
+		return;
+	end
+
+	local collisionMask1 = triggerComp1:GetCollisionMask();
+	local collisionMask2 = triggerComp2:GetCollisionMask();
+
+	if (collisionMask2 == CollisionMask.PLAYER) then
+		local rigidbody1 = gameObj1:GetRigidbodyComp();
+		if (rigidbody1 == nil) then
+			return;
+		end
+		local vel = Vector3.new(0,10,0);
+		rigidbody1:SetVelocity(vel);
+	
+	end
+
+	OutputPrint("OnEnter\n");
+	OutputPrint(">> " .. collisionMask1 .. "\n");
+	OutputPrint(">> " .. collisionMask2 .. "\n");
 
 end
 
